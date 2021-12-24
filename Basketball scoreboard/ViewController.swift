3cfc0c50db7eeb55b972e74fe594e7fef6bb4b9c
wei22 <weiwang24@icloud.com>
2021-12-24 21:29:13 +0800

    Initial Commit

---
+++ Basketball scoreboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Basketball scoreboard
+//
+//  Created by 王偉程 on 2021/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
